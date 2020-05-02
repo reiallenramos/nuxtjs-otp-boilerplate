62425d4ae7be2e0bea8948e80ed90b34e11ff67a
Rei Allen Ramos <reiallenramos@gmail.com>
2020-05-02 10:02:16 +0800

    login and register pages

---
+++ pages/login.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  v-container
+    h1 Login
+    UserAuthForm(buttonText="Login" :submitForm="loginUser")
+</template>
+
+<script>
+import UserAuthForm from '@/components/UserAuthForm'
+export default {
+  components: {
+    UserAuthForm
+  },
+  methods: {
+    loginUser() {
+      alert('You pressed a button')
+    }
+  }
+}
+</script>
---
+++ pages/register.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  v-container
+    h1 Register
+    UserAuthForm(buttonText="Register" :submitForm="registerUser" hasName="true")
+</template>
+
+<script>
+import UserAuthForm from '@/components/UserAuthForm'
+export default {
+  components: {
+    UserAuthForm
+  },
+  methods: {
+    registerUser() {
+      alert('You pressed a button')
+    }
+  }
+}
+</script>
