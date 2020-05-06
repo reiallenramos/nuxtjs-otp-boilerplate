b89dd0c1e81410f014b18001fb171f4f3cecb19b
Rei Allen Ramos <reiallenramos@gmail.com>
2020-05-06 13:44:41 +0800

    create seed task

--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ const { Nuxt, Builder } = require('nuxt')
 const app = express()
 const bodyParser = require('body-parser');
 const models = require('./models')
-const seeds = require('./seeds.json')
 
 app.use(bodyParser.json());
 
@@ -38,8 +37,6 @@ async function start () {
   app.use(nuxt.render)
 
   models.connectDb().then(async () => {
-    if(process.env.NODE_ENV !== 'production')  { seedUsers() }
-
     // Listen the server
     app.listen(port, host)
     consola.ready({
@@ -49,14 +46,3 @@ async function start () {
   });
 }
 start()
-
-const seedUsers = function() {
-  seeds.users.forEach(async user => {
-    try {
-      const u = new models.User(user);
-      await u.save();
-    } catch {
-      console.log(`email already exists: ${user.email}`)
-    }
-  })
-}
--- server/models/index.js
+++ server/models/index.js
@@ -2,11 +2,21 @@ const mongoose = require('mongoose')
 const config = require('../config')
 const { User } = require('./user')
 
+// address deprecations
+mongoose.set('useCreateIndex', true)
+mongoose.set('useUnifiedTopology', true)
+
 const connectDb = () => {
   return mongoose.connect(config.MONGO_URI);
 };
 
+const closeDb = () => {
+  console.log('Closing connection...')
+  return mongoose.connection.close();
+}
+
 module.exports = {
   connectDb,
+  closeDb,
   User,
 }
