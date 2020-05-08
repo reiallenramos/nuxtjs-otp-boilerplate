806c3be2c78ae75dfb9e51383c27bd28bcec3817
Rei Allen Ramos <reiallenramos@gmail.com>
2020-05-08 12:28:13 +0800

    install eslint and prettier

--- nuxt.config.js
+++ nuxt.config.js
@@ -96,6 +96,15 @@ module.exports = {
     ** You can extend webpack config here
     */
     extend (config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: "pre",
+          test: /\.(js|vue)$/,
+          loader: "eslint-loader",
+          exclude: /(node_modules)/
+        })
+      }
     }
   },
 
