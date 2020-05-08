6380fc3a936490f5a370d64d75d8b701131ca9f5
Rei Allen Ramos <reiallenramos@gmail.com>
2020-05-08 12:28:29 +0800

    lint validations.js

--- utils/validations.js
+++ utils/validations.js
@@ -1,22 +1,27 @@
 let required = (propertyType) => {
-  var article = (/^[aeiou]$/i).test(propertyType[0]) ? "an" : "a";
-  return v => v && v.length > 0 || `You must input ${article} ${propertyType}`
+  var article = /^[aeiou]$/i.test(propertyType[0]) ? "an" : "a"
+  return (v) =>
+    (v && v.length > 0) || `You must input ${article} ${propertyType}`
 }
 let minLength = (propertyType, minLength) => {
-  return v => v && v.length >= minLength || `${propertyType} must be at least ${minLength} characters`
+  return (v) =>
+    (v && v.length >= minLength) ||
+    `${propertyType} must be at least ${minLength} characters`
 }
 let maxLength = (propertyType, maxLength) => {
-  return v => v && v.length <= maxLength || `${propertyType} must be less than ${maxLength} characters`
+  return (v) =>
+    (v && v.length <= maxLength) ||
+    `${propertyType} must be less than ${maxLength} characters`
 }
 
 let emailFormat = () => {
-  let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,24})+$/
-  return v => v && regex.test(v) || "Must be a valid email"
+  let regex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,24})+$/
+  return (v) => (v && regex.test(v)) || "Must be a valid email"
 }
 
 export default {
   required,
   minLength,
   maxLength,
-  emailFormat
+  emailFormat,
 }
